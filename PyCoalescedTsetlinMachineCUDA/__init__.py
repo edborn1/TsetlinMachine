4dfde61a8e6b04e31379efeb4a8cedde6ec02725
edborn1 <153196180+edborn1@users.noreply.github.com>
2026-07-25 16:31:59 -0400

    Export Hypervector Tsetlin Machine classes

--- PyCoalescedTsetlinMachineCUDA/__init__.py
+++ PyCoalescedTsetlinMachineCUDA/__init__.py
@@ -1,1 +1,19 @@
+"""PyCoalescedTsetlinMachineCUDA package."""
 
+from .hypervector import (
+    BinaryFeatureHypervectorEncoder,
+    HypervectorMultiClassTsetlinMachine,
+    HypervectorMultiOutputTsetlinMachine,
+    HypervectorTsetlinMachine,
+    ProjectionDiagnostics,
+    SparseHypervectorSpace,
+)
+
+__all__ = [
+    "BinaryFeatureHypervectorEncoder",
+    "HypervectorMultiClassTsetlinMachine",
+    "HypervectorMultiOutputTsetlinMachine",
+    "HypervectorTsetlinMachine",
+    "ProjectionDiagnostics",
+    "SparseHypervectorSpace",
+]
